7ec0b99e32537cde8d58cfe2d7235bde1d9a8c93
André Aloisio Mendes <andre.mendes@serpro.gov.br>
2020-06-06 11:56:15 -0300

    Versão inicial

---
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,19 @@
+package br.com.alura.forum.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.alura.forum.model.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long>  {
+
+	List<Topico> findByCurso_Nome(String nomeCurso);
+	
+	
+	@Query("select t from Topico t where t.curso.nome = :nomeCurso")
+	List<Topico> porNomeCurso(@Param("nomeCurso") String nomeCurso);
+
+}
